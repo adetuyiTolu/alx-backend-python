ebbfd00f6a0230c7dd093835618015e1fa2db343
Tolu Adetuyi <tolu.adetuyi@gmail.com>
2025-07-11 13:58:23 -0400

    add logger function

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -4,7 +4,7 @@ import seed
 def stream_users():
     connection = seed.connect_to_prodev()
     cursor= connection.cursor()
-    cursor.execute("SELECT * FROM user_data")
+    cursor.execute(f"SELECT * FROM {seed.TABLE_NAME}")
     for row in cursor:
         yield row
     cursor.close()
