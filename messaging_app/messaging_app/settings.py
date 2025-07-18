e50c44b6503dee08008db2bce04759383cfffb6d
Tolu Adetuyi <tolu.adetuyi@gmail.com>
2025-07-18 13:26:29 -0400

    Update settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -71,16 +71,9 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-    ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
-    ],
-    'DEFAULT_PARSER_CLASSES': [
-        'rest_framework.parsers.JSONParser',
-    ],
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
 
 # Database
