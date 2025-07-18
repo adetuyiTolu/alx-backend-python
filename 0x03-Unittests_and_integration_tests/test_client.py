a9db44ad951beee4d5f352b8a52b482d2f0224fd
Tolu Adetuyi <tolu.adetuyi@gmail.com>
2025-07-18 12:59:51 -0400

    create integration testing

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,9 +3,10 @@
     Unit tests for client.py
 """
 import unittest
-from parameterized import parameterized
+from parameterized import parameterized, parameterized_class
 from unittest.mock import Mock, patch, PropertyMock
 from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -69,6 +70,52 @@ class TestGithubOrgClient(unittest.TestCase):
         """Unit test for has_license method"""
         result = GithubOrgClient.has_license(repo, license_key)
         self.assertEqual(result, expected)
+
+
+@parameterized_class([
+    {
+        'org_payload': org_payload,
+        'repos_payload': repos_payload,
+        'expected_repos': expected_repos,
+        'apache2_repos': apache2_repos,
+    }
+    for org_payload, repos_payload,
+    expected_repos, apache2_repos in TEST_PAYLOAD
+])
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """Integration test for GithubOrgClient.public_repos"""
+    @classmethod
+    def setUpClass(cls):
+        """Patch requests.get to mock external HTTP responses."""
+        cls.get_patcher = patch('requests.get')
+        mock_get = cls.get_patcher.start()
+
+        # Setup mock response per call
+        def side_effect(url):
+            mock_response = Mock()
+            if url.endswith("/orgs/google"):
+                mock_response.json.return_value = cls.org_payload
+            elif url.endswith("/orgs/google/repos"):
+                mock_response.json.return_value = cls.repos_payload
+            return mock_response
+
+        mock_get.side_effect = side_effect
+
+    @classmethod
+    def tearDownClass(cls):
+        """Stop patching requests.get"""
+        cls.get_patcher.stop()
+
+    def test_public_repos(self):
+        """Test public_repos returns expected list of repo names"""
+        client = GithubOrgClient("google")
+        self.assertEqual(client.public_repos(), self.expected_repos)
+
+    def test_public_repos_with_license(self):
+        """Test public_repos filters by Apache-2.0 license"""
+        client = GithubOrgClient("google")
+        self.assertEqual(client.public_repos(
+            license="apache-2.0"), self.apache2_repos)
 
 
 if __name__ == "__main__":
