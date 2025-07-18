aed3cb79ffc613f3ff6c44615abb1ea8e25c015e
Tolu Adetuyi <tolu.adetuyi@gmail.com>
2025-07-17 21:51:57 -0400

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -34,10 +34,10 @@ class TestAccessNestedMap(unittest.TestCase):
 class TestGetJson(unittest.TestCase):
      """Unit test for get_json"""
      @parameterized.expand([
-         ("test_example", "http://example.com", {"payload": True}),
-        ("test_holberton", "http://holberton.io", {"payload": False})
+         ("http://example.com", {"payload": True}),
+         ("http://holberton.io", {"payload": False}),
      ])
-     def test_get_json(self,name,test_url,test_payload):
+     def test_get_json(self,test_url,test_payload):
          """Test that get_json returns expected JSON payload."""
          mock_response = Mock()
          mock_response.json.return_value = test_payload
