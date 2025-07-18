3a55a44e6a283c2322d1dae8544aeaa0365a6d32
Tolu Adetuyi <tolu.adetuyi@gmail.com>
2025-07-17 20:56:08 -0400

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
+"""
+    Unit tests for utils.py
+"""
 
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
-    """_summary_
-        Unit test for access_nested_map
-    """
+    """Unit test for access_nested_map"""
+    
     @parameterized.expand([
          ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self,nested_map,path,expected):
+        """Test successful retrieval from nested map"""
         self.assertEqual(access_nested_map(nested_map,path),expected)
+        
+        
+
+if __name__ == "__main__":
+    unittest.main()
