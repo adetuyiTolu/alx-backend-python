295064991c21cc5f88adf06d243cd5d425a0fb44
Tolu Adetuyi <tolu.adetuyi@gmail.com>
2025-07-18 11:46:54 -0400

    create public repo test

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 
 import unittest
 from parameterized import parameterized
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, PropertyMock
 from client import GithubOrgClient
 
 
@@ -27,6 +27,18 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}"
         )
+
+    @patch("client.GithubOrgClient.org", new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        """Test the _public_repos_url property"""
+
+        expected_url = "https://api.github.com/orgs/google/repos"
+        mock_org.return_value = {"repos_url": expected_url}
+
+        client = GithubOrgClient("google")
+        result = client._public_repos_url
+
+        self.assertEqual(result, expected_url)
 
 
 if __name__ == "__main__":
