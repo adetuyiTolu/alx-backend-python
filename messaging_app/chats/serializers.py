7c33089d488112e25f02c440fa6ffc4c3615560e
Tolu Adetuyi <tolu.adetuyi@gmail.com>
2025-07-18 16:19:19 -0400

    Create serializers.py

---
+++ messaging_app/chats/serializers.py
@@ -0,0 +1,27 @@
+# messaging_app/serializers/user_serializer.py
+
+from rest_framework import serializers
+from models import User, Message, Conversation
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name',
+                  'email', 'phone_number', 'role']
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = ['id', 'sender', 'message_body', 'sent_at']
+
+
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    messages = MessageSerializer(
+        many=True, read_only=True, source='message_set')
+
+    class Meta:
+        model = Conversation
+        fields = ['id', 'participants', 'created_at', 'messages']
