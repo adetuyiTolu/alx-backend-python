2a05d4580687db71962f3d1f998ebb3c6b04ae70
Tolu Adetuyi <tolu.adetuyi@gmail.com>
2025-07-17 21:14:25 -0400

    added KeyError exception testing

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,7 +19,15 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test successful retrieval from nested map"""
         self.assertEqual(access_nested_map(nested_map,path),expected)
         
-        
+    @parameterized.expand([
+        ({},("a",)),
+        ({"a":1},("a","b"))
+    ])
+    def test_access_nested_map_exception(self,nested_map,path):
+        """Test KeyError exception"""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map,path)
+        self.assertEqual(str(context.exception), f"'{path[-1]}'")
 
 if __name__ == "__main__":
     unittest.main()
