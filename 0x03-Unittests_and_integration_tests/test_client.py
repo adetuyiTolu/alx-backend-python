727faa7c3a3c9651872f0358c164d7ed922567c9
Tolu Adetuyi <tolu.adetuyi@gmail.com>
2025-07-18 12:12:36 -0400

    create has license key test

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -61,6 +61,15 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_get.assert_called_once()
             mock_url.assert_called_once()
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Unit test for has_license method"""
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
