0725b331cd3c2d5bb2d59cea8c9ec8377ed93af9
Tolu Adetuyi <tolu.adetuyi@gmail.com>
2025-07-26 23:37:16 -0400

    update permission

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,3 +1,4 @@
+from .models import Conversation
 from rest_framework import permissions
 
 
@@ -11,4 +12,22 @@ class IsOwnerOrParticipant(permissions.BasePermission):
             return obj.user == request.user
         elif hasattr(obj, 'participants'):
             return request.user in obj.participants.all()
+        return False
+
+
+class IsParticipantOfConversation(permissions.BasePermission):
+    """
+    Custom permission to only allow participants of a conversation to access it.
+    """
+
+    def has_permission(self, request, view):
+        # Check that user is authenticated
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        # Check that the user is a participant in the conversation
+        if hasattr(obj, 'participants'):
+            return request.user in obj.participants.all()
+        elif hasattr(obj, 'conversation'):
+            return request.user in obj.conversation.participants.all()
         return False
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
-from .permissions import IsOwnerOrParticipant
+from .permissions import IsParticipantOfConversation
 
 from .models import Conversation, Message, CustomUserManager
 from .serializers import ConversationSerializer, MessageSerializer
@@ -13,7 +13,7 @@ from .serializers import ConversationSerializer, MessageSerializer
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrParticipant]
+    permission_classes = [IsParticipantOfConversation]
 
     def get_queryset(self):
         """Return only conversations the current user is part of."""
@@ -42,7 +42,7 @@ class ConversationViewSet(viewsets.ModelViewSet):
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrParticipant]
+    permission_classes = [IsParticipantOfConversation]
     filter_backends = [filters.OrderingFilter, filters.SearchFilter]
     ordering_fields = ['sent_at']
     search_fields = ['message_body']
