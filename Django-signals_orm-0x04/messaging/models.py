c1c68d142c5c40373ba2dd62aaa4c46af823cae5
Tolu Adetuyi <tolu.adetuyi@gmail.com>
2025-08-02 19:42:24 -0400

    add message history

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -7,6 +7,7 @@ class Message(models.Model):
     receiver = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)
 
 
 class Notification(models.Model):
@@ -14,3 +15,10 @@ class Notification(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
+
+
+class MessageHistory(models.Model):
+    message = models.ForeignKey(
+        Message, on_delete=models.CASCADE, related_name='history')
+    old_content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,6 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
 
 
 @receiver(post_save, sender=Message)
@@ -10,3 +10,19 @@ def create_notification(sender, instance, created, **kwargs):
             user=instance.receiver,
             message=instance
         )
+
+
+@receiver(pre_save, sender=Message)
+def log_message_history(sender, instance, **kwargs):
+    # This means the message already exists (i.e., being updated)
+    if instance.pk:
+        try:
+            old_instance = Message.objects.get(pk=instance.pk)
+            if old_instance.content != instance.content:
+                MessageHistory.objects.create(
+                    message=old_instance,
+                    old_content=old_instance.content
+                )
+                instance.edited = True
+        except Message.DoesNotExist:
+            pass
