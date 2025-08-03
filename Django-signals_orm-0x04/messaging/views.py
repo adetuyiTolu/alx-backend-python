4342ce615c57e645394378399a95a0fdbc3339c0
Tolu Adetuyi <tolu.adetuyi@gmail.com>
2025-08-02 20:01:43 -0400

    create delete user

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+@login_required
+def delete_user(request):
+    if request.method == 'POST':
+        request.user.delete()
+        return redirect('home')  # Redirect to homepage after deletion
